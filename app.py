0a5d0fcd4b9cfea0081fe43a47b5278d2aab550e
Yulduz Muradova <muradovayulduz1@gmail.com>
2023-04-23 10:00:14 -0400

    commit

--- app.py
+++ app.py
@@ -55,27 +55,31 @@ def leadz():
 def planz():
     return render_template('planz.html')
     
-    
-# Define frienz page route
-
+    # Define frienz page route
 @app.route('/frienz')
 def frienz():
     return render_template('frienz.html', friend_added=False)
 
-# Define add friend route to handle form submission
 @app.route('/add-friend', methods=['POST'])
 def add_friend():
     customer_id = request.form['customer_id']
+    nickname = request.form['nickname']  # get the nickname from the form
     account_found = False
     with open('accounts.json', 'r') as json_file:
         data = json.load(json_file)
         for row in data['results']:
             if row['customer_id'] == customer_id:
                 account_found = True
+                # add friend to customer's friend list with nickname
+                friends = row.get('friends', [])  # get the existing friends list or an empty list
+                friends.append({'customer_id': customer_id, 'nickname': nickname})  # add the new friend
+                row['friends'] = friends  # update the friends list
                 break
 
     if account_found:
-        # TODO: add friend to customer's friend list
+        # save the updated account data to the file
+        with open('accounts.json', 'w') as outfile:
+            json.dump(data, outfile)
         return render_template('frienz.html', friend_added=True)
     else:
         return render_template('frienz.html', friend_added=False)
