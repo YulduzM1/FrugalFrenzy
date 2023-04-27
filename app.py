b9c7a88c6d5c43f123c8a59b6d24f67673339f89
Yulduz Muradova <muradovayulduz1@gmail.com>
2023-04-26 21:51:38 -0400

    modified app.py added friends database to keep track of each friend tied to acount_id

--- app.py
+++ app.py
@@ -2,89 +2,104 @@ import csv
 import pandas as pd
 import requests
 import json
-from flask import Flask, render_template, request
+import mysql.connector
+from flask import Flask, render_template, request, redirect
+
 
 app = Flask(__name__)
+import mysql.connector
+
+mydb = mysql.connector.connect(
+    host='localhost',
+    user='root',
+    passwd='YulduzM1',
+    database='findb'
+)
+
+mycursor = mydb.cursor()
+sqlFormula = "INSERT INTO friends (name, friend_name) VALUES (%s, %s)"
 
 url = 'http://api.nessieisreal.com/enterprise/accounts?key=3ae6e71b194eff8c8c1cb81093705a6d'
-
-# fetch the account data from the API and save it to a CSV file
 response = requests.get(url)
 data = response.json()
-
-with open('accounts.json', 'w') as outfile:
-    json.dump(data, outfile)
-
 # Define home page route
 @app.route('/')
 def index():
     return render_template('index.html', account_found=False)
 
+
+# Define logout route to handle logout action
+@app.route('/logout')
+def logout():
+    return redirect('/')
+
 # Define login route to handle form submission
 @app.route('/login', methods=['POST'])
 def login():
-    customer_id = request.form['customer_id']
+    _id = request.form['_id']
     account_found = False
-    with open('accounts.json', 'r') as json_file:
-        data = json.load(json_file)
-        for row in data['results']:
-            if row['customer_id'] == customer_id:
-                account_found = True
-                break
+    nickname = ''
+    for row in data['results']:
+        if row['_id'] == _id:
+            account_found = True
+            nickname = row['nickname']
+            balance = row['balance']
+            break
 
     if account_found:
-        return render_template('Home.html', customer_id=customer_id)
+        return render_template('Home.html', _id=_id, nickname=nickname, balance=balance)
     else:
-        return render_template('index.html', account_found=False, customer_id=customer_id)
-
-
-# Define leadz page route
-@app.route('/home')
-def home():
-    return render_template('home.html')
-
-
-# Define leadz page route
-@app.route('/leadz')
-def leadz():
-    return render_template('leadz.html')
-
-
-# Define planz page route
-@app.route('/planz')
-def planz():
-    return render_template('planz.html')
+        return render_template('index.html', account_found=False, _id=_id)
     
+
+
 # Define frienz page route
-@app.route('/frienz')
-def frienz():
-    return render_template('frienz.html')
+# Define frienz page route
+@app.route('/frienz/<_id>', methods=['GET', 'POST'])
+def frienz(_id):
+    friend_name = None
+    if request.method == 'POST':
+        friend_name = request.form.get('friend_name')
+        if friend_name is None:
+            return render_template('frienz.html', friend_added=False, message='Please enter a friend name', _id=_id)
+        val = (_id, friend_name)
+        mycursor.execute("INSERT INTO friends (name, friend_name) VALUES (%s, %s)", val)
+        mydb.commit()
+    mycursor.execute("SELECT friend_name FROM friends WHERE name=%s", (_id,))
+    friends = [friend[0] for friend in mycursor.fetchall()]
+    return render_template('frienz.html', _id=_id, friends=friends, friend_name=friend_name, friend_added=True)
 
-@app.route('/add-friend', methods=['POST'])
-def add_friend():
-    customer_id = request.form['customer_id']
-    nickname = None
-    with open('accounts.json', 'r') as accounts_file:
-        accounts_data = json.load(accounts_file)
-        for account in accounts_data['results']:
-            if account['customer_id'] == customer_id:
-                nickname = account['nickname']
-                break
-    if nickname is None:
-        return render_template('frienz.html', friend_added=False, message='Customer not found')
+
+# Define home page route
+@app.route('/home/<_id>')
+def home(_id):
+    account_found = False
+    nickname = ''
+    balance = 0
+    for row in data['results']:
+        if row['_id'] == _id:
+            account_found = True
+            nickname = row['nickname']
+            balance = row['balance']
+            break
+
+    if account_found:
+        return render_template('Home.html', _id=_id, nickname=nickname, balance=balance)
     else:
-        friend_data = {
-            'customer_id': customer_id,
-            'nickname': nickname
-        }
-        with open('friends.json', 'r') as friends_file:
-            friends_data = json.load(friends_file)
-        friends_data['results'].append(friend_data)
-        with open('friends.json', 'w') as friends_file:
-            json.dump(friends_data, friends_file, indent=4)
-        return render_template('frienz.html', friend_added=True, message='Friend added successfully!')
+        return "Account not found"
 
 
+# Define leadz page route
+@app.route('/leadz/<_id>')
+def leadz(_id):
+    return render_template('leadz.html', _id=_id)
+
+
+# Define leadz page route
+@app.route('/planz/<_id>')
+def planz(_id):
+    return render_template('planz.html', _id=_id)
+    
 
 # Define about page route
 @app.route('/about')
