cc29cd8025a9e51cd84f952275fb01395e0ce21c
Yulduz Muradova <muradovayulduz1@gmail.com>
2023-04-30 04:10:26 -0400

    updated the planz route

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
-import csv
+
 import pandas as pd
 import requests
+import secrets
+
 import json
 import mysql.connector
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, flash, session
 
 
 app = Flask(__name__)
+app.secret_key = '123julia'
+
 import mysql.connector
 
 mydb = mysql.connector.connect(
@@ -95,11 +99,75 @@ def leadz(_id):
     return render_template('leadz.html', _id=_id)
 
 
-# Define leadz page route
-@app.route('/planz/<_id>')
+from datetime import datetime, timedelta
+
+@app.route('/planz/<_id>', methods=['GET', 'POST'])
 def planz(_id):
-    return render_template('planz.html', _id=_id)
-    
+    # Retrieve number of friends the user has
+    mycursor.execute("SELECT COUNT(*) FROM friends WHERE name=%s", (_id,))
+    num_friends = mycursor.fetchone()[0]
+
+    # Retrieve groups created by the user
+    mycursor.execute("SELECT * FROM communities WHERE leader_id=%s", (_id,))
+    communities = mycursor.fetchall()
+
+    # Handle form submission
+    if request.method == 'POST':
+        if 'create_group' in request.form:
+            if len(communities) == 0:
+                group_name = request.form.get('group_name')
+                amount_wanted = request.form.get('amount_wanted')
+                time_months = request.form.get('time_months')
+                first_payment_date_str = request.form.get('first_payment_date')
+
+                if not first_payment_date_str:
+                    first_payment_date = datetime.now() + timedelta(days=5)
+                else:
+                    first_payment_date = datetime.strptime(first_payment_date_str, '%Y-%m-%d')
+
+
+
+                # Calculate monthly plan amount
+                plan_amount = (int(amount_wanted) // int(time_months) // num_friends)
+
+                # Insert new group into database
+                sql = "INSERT INTO communities (group_name, leader_id, amount_wanted, time_months, first_payment_date) VALUES (%s, %s, %s, %s, %s)"
+                val = (group_name, _id, amount_wanted, time_months, first_payment_date)
+                mycursor.execute(sql, val)
+                mydb.commit()
+
+                # Insert monthly payments into database
+                for i in range(int(time_months)):
+                    payment_date = first_payment_date + timedelta(days=30*i)
+                    sql = "INSERT INTO payments (group_id, amount, member_id, payment_date) VALUES (%s, %s, %s, %s)"
+                    val = (mycursor.lastrowid, plan_amount, _id, payment_date)
+                    mycursor.execute(sql, val)
+                    mydb.commit()
+
+                # Redirect to the same page to refresh the number of groups and show a success message
+                flash("Group created successfully!")
+                return redirect('/planz/{}'.format(_id))
+            else:
+                flash("You can only create one group.")
+        elif 'delete_group' in request.form:
+            group_id = request.form.get('group_id')
+
+            # Delete the group and payments from the database
+            sql = "DELETE FROM communities WHERE group_id=%s"
+            val = (group_id,)
+            mycursor.execute(sql, val)
+
+            sql = "DELETE FROM payments WHERE group_id=%s"
+            val = (group_id,)
+            mycursor.execute(sql, val)
+
+            mydb.commit()
+
+            # Redirect to the same page to refresh the list of groups and show a success message
+            flash("Group deleted successfully!")
+            return redirect('/planz/{}'.format(_id))
+
+    return render_template('planz.html', _id=_id, num_friends=num_friends, communities=communities)
 
 # Define about page route
 @app.route('/about')
