aeed98b057d4447cf5cbacd2b0141ad24ba6c3c5
Yulduz Muradova <muradovayulduz1@gmail.com>
2023-05-19 11:39:22 -0400

    changed the port to 80

--- app.py
+++ app.py
@@ -309,4 +309,4 @@ def about():
     return render_template('about.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000, debug=True)
+    app.run(host='0.0.0.0', port=80, debug=True)
